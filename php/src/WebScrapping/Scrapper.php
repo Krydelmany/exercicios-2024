33a62d237e5618327d62ce3c00c16701c6220582
Giovani Claro Moraes <giclamoraes@gmail.com>
2024-04-25 17:17:07 -0300

    Testes Automatizados 5.1 (Lasts Fix Main and Scrapper)

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -19,8 +19,11 @@ class Scrapper {
   /**
    * Loads paper information from the HTML, writes the data into a XLSX file.
    *
-   * @param \DOMDocument $dom  The DOMDocument object representing the HTML structure of the webpage.
-   * @param string $path  The path where the XLSX file will be saved.
+   * @param \DOMDocument $dom  The DOMDocument object representing the HTML.
+   *   structure of the webpage.
+   * 
+   * @param string $path  The path.
+   *   where the XLSX file will be saved.
    */
   public function scrapAndWriteXlsx(\DOMDocument $dom, string $path) : void {
 
@@ -128,7 +131,7 @@ class Scrapper {
       'Author 8 Institution',
       'Author 9',
       'Author 9 Institution',
-      ];
+    ];
 
     // Format data rows.
     foreach ($data as $paper) {
