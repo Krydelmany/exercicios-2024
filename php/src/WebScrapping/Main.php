ce18330f76e81add4314833596a654bf3ddfd5ae
Giovani Claro Moraes <giclamoraes@gmail.com>
2024-04-25 00:35:59 -0300

    Test2

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -3,19 +3,20 @@
 namespace Chuva\Php\WebScrapping;
 
 /**
- * Runner for the Webscrapping exercice.
+ * Runner for the Webscrapping exercise.
  */
-class Main {
+class Main
+{
+    /**
+     * Main runner, instantiates a Scrapper and runs.
+     */
+    public static function run(): void
+    {
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
-  /**
-   * Main runner, instantiates a Scrapper and runs.
-   */
-  public static function run(): void {
-    $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
-    
-    $scrapper = new Scrapper();
-    
-    $scrapper->scrapAndWriteXlsx($dom, 'assets/data_origin.xlsx');
-  }
+        $scrapper = new Scrapper();
+
+        $scrapper->scrapAndWriteXlsx($dom, 'assets/data_origin.xlsx');
+    }
 }
